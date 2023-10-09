2ec74675219ab92e1d2c75aee4e6ac02999573e3
Bruno Freire <brunofreire1717@gmail.com>
2023-10-09 08:24:02 -0500

    feature: add swagger in route /api

--- src/modules/users/application/users.controller.ts
+++ src/modules/users/application/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { User } from '../domain/user';
 import { UsersService } from './users.service';
+import { ApiBody } from '@nestjs/swagger';
+import { ICreateUserDTO } from '../dto/ICreateUserDTO';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +14,8 @@ export class UsersController {
   }
 
   @Post()
-  create(@Body() user: User): User {
+  @ApiBody({type: ICreateUserDTO})
+  create(@Body() user: ICreateUserDTO): User {
     return this.usersService.create(user)
   }
 }
--- src/modules/users/dto/ICreateUserDTO.ts
+++ src/modules/users/dto/ICreateUserDTO.ts
@@ -1,6 +1,21 @@
-export interface ICreateUserDTO {
-  name: string,
-  email: string,
-  phone: string,
+import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+
+export class ICreateUserDTO {
+  @ApiProperty()
+  @IsString()
+  name: string
+
+  @ApiProperty()
+  @IsEmail()
+  email: string
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  phone: string
+
+  @ApiProperty()
+  @IsNotEmpty()
   password: string
 }
